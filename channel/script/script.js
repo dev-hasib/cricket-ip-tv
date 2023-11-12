6b4ca2ad2df8f4a09c4fc851cc0d8474f16addd2
AL HASIB <hasib2653@gmail.com>
2023-11-12 22:54:23 +0600

    update some features and add a new branch

---
+++ channel/script/script.js
@@ -0,0 +1,23 @@
+const iframe = document.getElementById('myIframe');
+const main = document.getElementById('main');
+function toggleFullscreen() {
+	if (!document.fullscreenElement) {
+		// If the iframe is not currently in fullscreen mode, go fullscreen
+		iframe.requestFullscreen().catch((err) => {
+			console.error('Error attempting to enable fullscreen:', err);
+		});
+	} else {
+		// If the iframe is currently in fullscreen mode, exit fullscreen
+		document.exitFullscreen();
+	}
+}
+const da = document.getElementById('da');
+da.addEventListener('click', function () {
+	if (iframe.classList[0] !== 'ab') {
+		iframe.classList.add('ab');
+		da.innerHTML = 'Enabled ads';
+	} else {
+		iframe.classList.remove('ab');
+		da.innerHTML = 'Disabled ads';
+	}
+});
